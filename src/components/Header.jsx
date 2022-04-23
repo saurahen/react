26a6a7ada60aaf15a87873aeffe3796e7d7e4301
Pavel Tiunov <saurahen@gmail.com>
2022-04-23 14:04:06 +0700

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className='header'>
+      <img
+        src='https://upload.wikimedia.org/wikipedia/commons/1/1e/RPC-JP_Logo.png'
+        className='header_img'
+      />
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <section className='content'>
+      <img
+        src='https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg'
+        className='content_img'
+      />
+      <div> Ava + description</div>
+      <div>
+        My post
+        <div>New Post</div>
+      </div>
+      <div>
+        <div>Post 1</div>
+        <div>Post 2</div>
+        <div>Post 3</div>
+        <div>Post 4</div>
+      </div>
+    </section>
+  );
+};
+
+export default Profile;
